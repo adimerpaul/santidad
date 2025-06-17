6affde7d934445924e6b1502a7a941332edd0747
adimerpaul <adimer101@gmail.com>
2025-06-16 21:00:36 -0400

    Actulziado seo

--- frontTienda/quasar.config.js
+++ frontTienda/quasar.config.js
@@ -90,6 +90,9 @@ module.exports = configure(function (/* ctx */) {
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#framework
     framework: {
+      plugins: [
+        'Meta'
+      ],
       config: {},
 
       // iconSet: 'material-icons', // Quasar icon set
--- frontTienda/src/pages/products/DetalleProducto.vue
+++ frontTienda/src/pages/products/DetalleProducto.vue
@@ -1,209 +1,114 @@
 <template>
-<q-page class="bg-grey-3 q-pa-md">
-  <div class="row">
-    <div class="col-12">
-      <q-breadcrumbs>
-        <q-breadcrumbs-el label="Inicio" to="/" />
-        <q-breadcrumbs-el label="Productos" to="/" />
-        <q-breadcrumbs-el :label="product?.nombre" />
-      </q-breadcrumbs>
-    </div>
-    <div :class="`col-12 col-md-6 ${$q.screen.lt.md?'text-center':'text-right'}`">
-      <q-img
-        :src="product?.imagen.includes('http')?product?.imagen:`${$url}../images/${product?.imagen}`"
-        spinner-color="white"
-        v-if="product?.imagen"
-        style="border: 2px solid #496aec;border-radius: 5px"
-        width="250px"
-        spinner-size="40px"></q-img>
-    </div>
-<!--    {-->
-<!--    "id": 8,-->
-<!--    "nombre": "HUGGIES ONE DONE REFRESHING TOALLITAS HUMEDAS X 48 UNIDADES",-->
-<!--    "barra": null,-->
-<!--    "cantidad": 0,-->
-<!--    "cantidadAlmacen": 0,-->
-<!--    "cantidadSucursal1": 0,-->
-<!--    "cantidadSucursal2": 0,-->
-<!--    "cantidadSucursal3": 0,-->
-<!--    "cantidadSucursal4": 0,-->
-<!--    "costo": 15.38,-->
-<!--    "precioAntes": 29.5,-->
-<!--    "precio": 20,-->
-<!--    "activo": "ACTIVO",-->
-<!--    "unidad": "PAQUETE",-->
-<!--    "registroSanitario": null,-->
-<!--    "paisOrigen": "",-->
-<!--    "nombreComun": null,-->
-<!--    "composicion": null,-->
-<!--    "marca": "",-->
-<!--    "distribuidora": "",-->
-<!--    "imagen": "7702425800700_a34898b6-0419-4af4-ac82-6dcdfea028f7_543x543.webp",-->
-<!--    "descripcion": "Las toallitas humedas Huggies naturally refreshing baby tienen un aroma a pepino fresco y te verde con una textura similar a una toallita. Estas toallitas no contienen alcohol y contienen ingredientes suaves como el aloe por lo que puede usar estas toallitas para limpiar mas que solo las nalgas de su bebe.",-->
-<!--    "category_id": 6,-->
-<!--    "agencia_id": 1,-->
-<!--    "created_at": "2023-08-18T10:47:56.000000Z",-->
-<!--    "updated_at": "2024-05-31T09:32:35.000000Z",-->
-<!--    "subcategory_id": 24,-->
-<!--    "cantidadSucursal5": 0,-->
-<!--    "cantidadSucursal6": 0,-->
-<!--    "cantidadSucursal7": 0,-->
-<!--    "cantidadSucursal8": 0,-->
-<!--    "cantidadSucursal9": 0,-->
-<!--    "cantidadSucursal10": 0-->
-<!--    }-->
-    <div class="col-12 col-md-6">
-      <div class="text-left q-px-md">
-        <div class="text-h6">{{product?.nombre}}</div>
-        <table>
-          <tr>
-            <td class="text-bold">Precio:</td>
-            <td>
-              Bs. {{product?.precio}}
-              <q-badge v-if="es_porcentaje" color="red">
-                Descuento {{product?.porcentaje}}%
-                - Bs. {{(product?.precio * product?.porcentaje / 100).toFixed(2)}}
-              </q-badge>
-              <span>
-                <q-badge color="green" v-if="es_porcentaje">
-                  Bs. {{product?.precioNormal}}
-                </q-badge>
-              </span>
-<!--              <pre>{{product}}</pre>-->
-            </td>
-          </tr>
-          <tr>
-            <td class="text-bold">Unidad:</td>
-            <td>{{product?.unidad}}</td>
-          </tr>
-          <tr>
-            <td class="text-bold">Stock:</td>
-            <td>{{product?.cantidad > 100 ? '100' : product?.cantidad}}</td>
-          </tr>
-          <tr>
-            <td class="text-bold">Principio activo:</td>
-            <td>{{product?.composicion}}</td>
-          </tr>
-          <tr>
-            <td class="text-bold">Distribuidora:</td>
-            <td>{{product?.distribuidora}}</td>
-          </tr>
-          <tr>
-            <td class="text-bold">Laboratorio:</td>
-            <td>{{product?.marca}}</td>
-          </tr>
-          <tr>
-            <td class="text-bold">Pais origen:</td>
-            <td>{{product?.paisOrigen}}</td>
-          </tr>
-          <tr>
-            <td class="text-bold">Registro sanitario:</td>
-            <td>{{product?.registroSanitario}}</td>
-          </tr>
-        </table>
+  <q-page class="bg-grey-3 q-pa-md">
+    <div class="row">
+      <div class="col-12">
+        <q-breadcrumbs>
+          <q-breadcrumbs-el label="Inicio" to="/" />
+          <q-breadcrumbs-el label="Productos" to="/" />
+          <q-breadcrumbs-el :label="product?.nombre" />
+        </q-breadcrumbs>
       </div>
-      <div class="row">
-        <div class="col-9 flex flex-center">
-          <span class="text-bold">Cantidad:</span>
-          <q-btn
-            @click="cantidad = cantidad - 1"
-            icon="remove"
-            dense
-            round
-            color="primary"
-            :disable="cantidad<=1"
-          />
-          <q-input v-model="cantidad" type="number"  min="1" class="text-center text-bold"  outlined dense style="text-align: center;width: 100px"/>
-          <q-btn
-            @click="cantidad = cantidad + 1"
-            icon="add"
-            dense
-            round
-            color="primary"
-            :disable="cantidad>=product?.cantidad"
-          />
-          <span class="text-bold">{{product?.unidad}}</span>
+
+      <div :class="`col-12 col-md-6 ${$q.screen.lt.md ? 'text-center' : 'text-right'}`">
+        <q-img
+          :src="product?.imagen.includes('http') ? product?.imagen : `${$url}../images/${product?.imagen}`"
+          spinner-color="white"
+          v-if="product?.imagen"
+          style="border: 2px solid #496aec; border-radius: 5px"
+          width="250px"
+          spinner-size="40px"
+        />
+      </div>
+
+      <div class="col-12 col-md-6">
+        <div class="text-left q-px-md">
+          <h1 class="text-h5 q-mb-md">{{ product?.nombre }}</h1>
+
+          <h2 class="text-h6 q-mt-sm">Precio y características</h2>
+          <table>
+            <tr>
+              <td class="text-bold">Precio:</td>
+              <td>
+                Bs. {{ product?.precio }}
+                <q-badge v-if="es_porcentaje" color="red">
+                  Descuento {{ product?.porcentaje }}% - Bs. {{ (product?.precio * product?.porcentaje / 100).toFixed(2) }}
+                </q-badge>
+                <q-badge color="green" v-if="es_porcentaje">
+                  Bs. {{ product?.precioNormal }}
+                </q-badge>
+              </td>
+            </tr>
+            <tr><td class="text-bold">Unidad:</td><td>{{ product?.unidad }}</td></tr>
+            <tr><td class="text-bold">Stock:</td><td>{{ product?.cantidad > 100 ? '100' : product?.cantidad }}</td></tr>
+            <tr><td class="text-bold">Principio activo:</td><td>{{ product?.composicion }}</td></tr>
+            <tr><td class="text-bold">Distribuidora:</td><td>{{ product?.distribuidora }}</td></tr>
+            <tr><td class="text-bold">Laboratorio:</td><td>{{ product?.marca }}</td></tr>
+            <tr><td class="text-bold">País origen:</td><td>{{ product?.paisOrigen }}</td></tr>
+            <tr><td class="text-bold">Registro sanitario:</td><td>{{ product?.registroSanitario }}</td></tr>
+          </table>
         </div>
-        <div class="col-3 bg-grey-4 text-right">
-          <label class="text-bold ">Total</label>
-          <div class="text-bold justify-between">
-            Bs.
-            <span>{{(product?.precio * cantidad).toFixed(2)}}</span>
+
+        <div class="row">
+          <div class="col-9 flex flex-center">
+            <span class="text-bold">Cantidad:</span>
+            <q-btn @click="cantidad = cantidad - 1" icon="remove" dense round color="primary" :disable="cantidad <= 1" />
+            <q-input v-model="cantidad" type="number" min="1" class="text-center text-bold" outlined dense style="width: 100px" />
+            <q-btn @click="cantidad = cantidad + 1" icon="add" dense round color="primary" :disable="cantidad >= product?.cantidad" />
+            <span class="text-bold">{{ product?.unidad }}</span>
+          </div>
+
+          <div class="col-3 bg-grey-4 text-right">
+            <label class="text-bold">Total</label>
+            <div class="text-bold">Bs. <span>{{ (product?.precio * cantidad).toFixed(2) }}</span></div>
+          </div>
+
+          <div class="col-4 q-pa-xs">
+            <q-btn @click="addCarrito(product, cantidad)" label="Añadir al carrito" icon="add_shopping_cart" class="full-width" color="blue" no-caps dense />
+          </div>
+          <div class="col-4 q-pa-xs">
+            <q-btn @click="addCart(product, cantidad, true)" label="Comprar ahora" icon="shopping_cart" class="full-width" color="red" no-caps dense />
+          </div>
+          <div class="col-4 q-pa-xs">
+            <q-btn @click="share" label="Compartir" icon="share" class="full-width" color="green" no-caps dense />
           </div>
         </div>
-        <div class="col-4 q-pa-xs">
-          <q-btn
-            @click="addCarrito(product, cantidad)"
-            label="Añadir al carrito"
-            icon="add_shopping_cart"
-            class="full-width"
-            color="blue"
-            no-caps
-            dense/>
-        </div>
-        <div class="col-4 q-pa-xs">
-          <q-btn
-            @click="addCart(product, cantidad, true)"
-            label="Comprar ahora"
-            icon="shopping_cart"
-            class="full-width"
-            color="red"
-            no-caps
-            dense/>
-        </div>
-        <div class="col-4 q-pa-xs">
-          <q-btn
-            @click="share"
-            label="Compartir"
-            icon="share"
-            class="full-width"
-            color="green"
-            no-caps
-            dense/>
+
+        <div class="q-pt-xs">
+          <h3 class="text-h6 text-blue">Disponible en las siguientes sucursales</h3>
+          <q-list dense>
+            <template v-for="sucursal in sucursales" :key="sucursal.id">
+              <q-item v-if="sucursal.cantidad > 0">
+                <q-item-section avatar>
+                  <q-avatar><q-icon name="store" /></q-avatar>
+                </q-item-section>
+                <q-item-section>
+                  <q-item-label>{{ sucursal.nombre }}</q-item-label>
+                  <q-item-label caption>{{ sucursal.direccion }}</q-item-label>
+                </q-item-section>
+                <q-item-section side>
+                  <q-badge color="green" :label="'Disponible'" />
+                </q-item-section>
+              </q-item>
+            </template>
+          </q-list>
         </div>
       </div>
-      <div class="q-pt-xs">
-        <div class="text-bold text-blue">Disponible en las siguientes sucursales:</div>
-        <q-list dense>
-          <template v-for="sucursal in sucursales" :key="sucursal.id">
-          <q-item
-            clickable
-            v-if="sucursal.cantidad > 0"
-          >
-            <q-item-section avatar>
-              <q-avatar>
-                <q-icon name="store" />
-              </q-avatar>
-            </q-item-section>
-            <q-item-section>
-              <q-item-label>
-                {{ sucursal.nombre }}
-              </q-item-label>
-              <q-item-label caption>
-                {{ sucursal.direccion }}
-              </q-item-label>
-            </q-item-section>
-            <q-item-section side>
-              <q-badge color="green" :label="'Disponible'" />
-            </q-item-section>
-          </q-item>
-          </template>
-        </q-list>
+
+      <div class="col-12">
+        <h2 class="text-h6">Descripción</h2>
+        <div class="text-justify q-px-md" v-html="product?.descripcion"></div>
+      </div>
+
+      <div class="col-12">
+        <h3 class="text-h6">Productos relacionados</h3>
       </div>
     </div>
-    <div class="col-12">
-      <label class="text-bold text-h6">Descripción:</label>
-      <div class="text-justify q-px-md" v-html="product?.descripcion"></div>
-    </div>
-    <div class="col-12">
-      <label class="text-bold text-h6">Productos relacionados:</label>
-    </div>
-  </div>
-<!--  <pre>{{product}}</pre>-->
-<!--  <pre>{{sucursales}}</pre>-->
-</q-page>
+  </q-page>
 </template>
+
 <script>
+import { useMeta } from 'quasar'
+
 export default {
   name: 'DetalleProducto',
   data () {
@@ -219,6 +124,30 @@ export default {
   async mounted () {
     await this.getSucursales()
     await this.getProduct()
+    // const nombre = this.product.nombre || 'Detalle del Producto'
+    // const descripcion = this.product?.descripcion || 'Explora nuestros productos disponibles en farmacia online.'
+    // const imagen = this.product.imagen?.includes('http')
+    //   ? this.product.imagen
+    //   : `${window.location.origin}/images/${this.product.imagen}`
+    //
+    // useMeta({
+    //   title: nombre,
+    //   titleTemplate: title => `${title} - Santidad Divina`,
+    //   description: descripcion,
+    //   meta: {
+    //     description: { name: 'description', content: 'Page 1' },
+    //     keywords: { name: 'keywords', content: 'Quasar website' },
+    //     equiv: { 'http-equiv': 'Content-Type', content: 'text/html; charset=UTF-8' },
+    //     'og:title': { property: 'og:title', content: nombre },
+    //     'og:description': { property: 'og:description', content: descripcion },
+    //     'og:image': { property: 'og:image', content: imagen },
+    //     'og:type': { property: 'og:type', content: 'product' },
+    //     'og:url': { property: 'og:url', content: window.location.href }
+    //   },
+    //   link: {
+    //     canonical: { rel: 'canonical', href: window.location.href }
+    //   }
+    // })
   },
   methods: {
     async getSucursales () {
@@ -241,6 +170,28 @@ export default {
       })
       this.loading = false
       this.$q.loading.hide()
+      console.log('descripcion', this.product?.descripcion)
+      const descripcionSeo = this.product?.descripcion + ' Producto disponible en Santidad Divina. Encuentra este y otros productos médicos en nuestra farmacia digital, con stock actualizado y atención inmediata.' || 'Producto disponible en Santidad Divina.'
+      useMeta({
+        title: this.product.nombre,
+        description: descripcionSeo,
+        meta: {
+          description: { name: 'description', content: descripcionSeo },
+          keywords: {
+            name: 'keywords',
+            content: `medicamento, salud, ${this.product.nombre}, ${this.product.marca}, ${this.product?.composicion}`
+          },
+          robots: { name: 'robots', content: 'index, follow' },
+          'og:title': { property: 'og:title', content: this.product.nombre },
+          'og:description': { property: 'og:description', content: descripcionSeo },
+          'og:image': { property: 'og:image', content: this.$url + '../images/' + this.product.imagen },
+          'og:type': { property: 'og:type', content: 'product' },
+          'og:url': { property: 'og:url', content: window.location.href }
+        },
+        link: {
+          canonical: { rel: 'canonical', href: window.location.href }
+        }
+      })
     },
     addCarrito (product, cantidad) {
       product.cantidad = cantidad
